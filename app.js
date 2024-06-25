266d86afb5cec22a1114e047a0cc5001af7cb544
Randyx233 <lailihost@gmail.com>
2024-06-25 22:45:53 +0800

    init

---
+++ app.js
@@ -0,0 +1,29 @@
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/valyrian.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("The server is rate limit. Try again after some time.");
+    
+}
+
+function btnClick() {
+    var inputText = txtInput.value; 
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated
+            outputDiv.innerText = translatedText
+        })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", btnClick);
